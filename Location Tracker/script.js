ec3fec860aaaf608b22a059876d6aa893951604d
Shreeram Singraul <aashikrajhpoot@gmail.com>
2025-07-28 18:44:15 +0530

    Delete Location Tracker directory

--- Location Tracker/script.js
+++
@@ -1,9 +0,0 @@
-let h1 = document.querySelector("#h1");
-
-window.addEventListener("keydown", (e) => {
-  if (e.key === " ") {
-    h1.textContent = "Space"
-  } else {
-    h1.textContent = e.key;
-  }
-});
